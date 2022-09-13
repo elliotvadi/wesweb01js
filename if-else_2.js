9945357b73da3102af9c5bbb2fa74742d76815ba
Elliot Ozolins <45043033+elliotvadi@users.noreply.github.com>
2022-09-13 10:39:52 +0200

    Add files via upload

---
+++ if-else_2.js
@@ -0,0 +1,18 @@
+// Elliot Vadi, TE20D
+// Subject: Check whether or not a given year is a leap year.
+
+console.log("Please enter a year.");
+let y = prompt("Year: ");
+
+if (y % 400 == 0) {
+    console.log(y + " is a leap year.");
+}
+else if (y % 100 == 0) {
+    console.log(y + " is not a leap year.");
+}
+else if (y % 4 == 0) {
+    console.log(y + " is a leap year.");
+}
+else {
+    console.log(y + " is not a leap year.");
+}
