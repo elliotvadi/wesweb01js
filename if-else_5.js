e71a7afef6c3362900fda003353d2cc526ed23f3
Elliot Ozolins <45043033+elliotvadi@users.noreply.github.com>
2022-09-13 12:51:49 +0200

    Add files via upload

---
+++ if-else_5.js
@@ -0,0 +1,18 @@
+// Elliot Vadi, TE20D
+// Subject: Program takes three numbers and outputs them in order of size.
+
+var ps = require("prompt-sync");
+var prompt = ps();
+
+console.log("You'll be asked to input three (3) numbers. The program will then output them in order of size.");
+
+// The numbers to take as input
+let num1 = prompt("Number 1: ");
+let num2 = prompt("Number 2: ");
+let num3 = prompt("Number 3: ");
+
+const numbers = [num1, num2, num3];
+
+numbers.sort(function(a, b){return a - b});
+
+console.log(numbers);
