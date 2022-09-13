99fe38e6aed4c9a351efa0dcb628af23d31ecfe3
Elliot Ozolins <45043033+elliotvadi@users.noreply.github.com>
2022-09-13 12:49:57 +0200

    Update if-else_4.js

--- if-else_4.js
+++ if-else_4.js
@@ -1,5 +1,5 @@
 // Elliot Vadi, TE20D
-// Subject: Program takes three numbers and outputs them in order of size.
+// Subject: Program takes three numbers and outputs the biggest number.
 
 var ps = require("prompt-sync");
 var prompt = ps();
@@ -13,6 +13,6 @@ let num3 = prompt("Number 3: ");
 
 const numbers = [num1, num2, num3];
 
-numbers.sort(function(a, b){return a - b});
+numbers.sort(function(a, b){return a + b});
 
-console.log(numbers);
+console.log(numbers[0]);
